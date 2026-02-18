b3586248b32700b26ddd7aff025e79d76fbea590
parmarhitesh1128-svg <parmarhitesh1128@gmail.com>
2026-02-18 08:58:14 +0530

    Add files via upload

---
+++ dic2.py
@@ -0,0 +1,18 @@
+'''d={[1,2,3,4]:2,[5,6,7,8]:3}
+d={2:[1,2,3,4],3:[5,6,7,8]}
+print(d)
+d[2]="python"
+print(d)'''
+'''d={1:"python",2:"sql",3:"java"}
+print(d)
+d[4]="AI"
+print(5)={"ml","dl","nlp"}
+prin(d)
+d[5]=3
+print(6)'''
+a=[1,2,3,4,5]
+b=["a","b","c","e"]
+print("A->",a)
+print("B->",b)
+d={key:value for key,value in zip(a,b)}
+print(d)
